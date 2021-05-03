23bc5d65ae5bd649134628988cfd1bfa710ac7b7
Full Name <email@example.com>
2021-05-03 13:55:56 +0300

    photo

--- api/prog.py
+++ api/prog.py
@@ -5,5 +5,5 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(https://www.denofgeek.com/wp-content/uploads/2020/09/Sacha-Baron-Cohen-as-Borat.jpg?fit=1200%2C804.encode())
+    self.wfile.write('<img src="https://www.denofgeek.com/wp-content/uploads/2020/09/Sacha-Baron-Cohen-as-Borat.jpg?fit=1200%2C804"/>'.encode())
     return
