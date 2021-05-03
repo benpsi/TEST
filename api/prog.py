0b8fcd6783c2a653a93bc38452f94bf7d8b18e30
Full Name <email@example.com>
2021-05-03 13:57:45 +0300

    photo

--- api/prog.py
+++ api/prog.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/html')
     self.end_headers()
-    self.wfile.write('<img src="https://www.denofgeek.com/wp-content/uploads/2020/09/Sacha-Baron-Cohen-as-Borat.jpg?fit=1200%2C804"/>'.encode())
+    self.wfile.write('<img src="data:image/jpeg;base64,https://www.denofgeek.com/wp-content/uploads/2020/09/Sacha-Baron-Cohen-as-Borat.jpg?fit=1200%2C804"/>'.encode())
     return
